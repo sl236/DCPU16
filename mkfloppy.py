99d7d5355f1d7960500c29be0223c0b8cc3ceb5a
Sergei Lewis <moonshadow@toothycat.net>
2012-05-14 16:56:47 +0100

    FDD WIP

--- mkfloppy.py
+++ mkfloppy.py
@@ -18,6 +18,13 @@ def readfile(path):
 		result = result + ( (SECTOR_SIZE-(len(result) & (SECTOR_SIZE-1))) * b'\x00' )
 	return result			
 
+def readshort( src, word_offset ):
+	return (src[word_offset*2]<<8) | src[word_offset*2+1]
+
+def writeshort( dest, word_offset, data ):
+	dest[word_offset*2] = (data >> 8) & 0xFF
+	dest[word_offset*2+1] = data & 0xFF
+
 # ------------------------------
 # check prerequisites
 
@@ -29,10 +36,6 @@ if not os.access('bootsector.dasm16', os.F_OK):
 	print >> sys.stderr, 'bootsector.dasm16 not found!'
 	sys.exit(-1)
 
-if not os.access('bootsector.bin', os.W_OK):
-	print >> sys.stderr, 'bootsector.bin cannot be written!'
-	sys.exit(-1)
-	
 os.system('./assemble bootsector.dasm16 -Oxxd | xxd -r > bootsector.bin')
 
 if not os.access('bootsector.bin', os.R_OK):
@@ -42,4 +45,10 @@ if not os.access('bootsector.bin', os.R_OK):
 # ------------------------------
 # create initial filesystem
 
-bootsector = readfile('bootsector.bin')
+fs = readfile('bootsector.bin')
+fs[SECTOR_SIZE-4] = b'M'
+fs[SECTOR_SIZE-3] = b'o'
+fs[SECTOR_SIZE-2] = b'o'
+fs[SECTOR_SIZE-1] = b'n'
+
+fs = fs + bytearray((SECTOR_SIZE * 2) * b'\0')	# root inode and bitmap
